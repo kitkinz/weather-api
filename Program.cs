9b576ad2de7fdc6207b1208981189165b39434b1
kitkinz <kitkinzpanawan@gmail.com>
2025-09-18 09:40:01 +0800

    added local Redis caching thru Docker

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
+using StackExchange.Redis;
 using WeatherApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Framework essentials
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+// Application services
 builder.Services.AddHttpClient<WeatherService>();
+
+// Infrastructure
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var configuration = new ConfigurationOptions
+    {
+        EndPoints = { { "redis-10926.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com", 10926 } },
+        User = "default",
+        Password = "KulCAxwRt3RHj313Z7QrqcJT7JTQkVp2",
+        AbortOnConnectFail = false,
+        Ssl = true,
+    };
+
+    return ConnectionMultiplexer.Connect("localhost");
+});
+
+builder.Services.AddSingleton<IDatabase>(sp =>
+{
+    var muxer = sp.GetRequiredService<IConnectionMultiplexer>();
+    return muxer.GetDatabase();
+});
 
 var app = builder.Build();
 
--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -1,4 +1,6 @@
 using WeatherApi.Models;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace WeatherApi.Services;
 
@@ -6,6 +8,7 @@ public class WeatherService
 {
     private readonly HttpClient _httpClient;
     private readonly string _apiKey;
+    private readonly IDatabase _redis;
 
     private static readonly string[] summaries =
     {
@@ -23,20 +26,37 @@ public class WeatherService
     //     ));
     // }
 
-    public WeatherService(HttpClient httpClient)
+    public WeatherService(HttpClient httpClient, IDatabase redis)
     {
         _httpClient = httpClient;
         _apiKey = Environment.GetEnvironmentVariable("VISUAL_CROSSING_API_KEY")
                 ?? throw new InvalidOperationException("API key not found in environment variables.");
+        _redis = redis;
     }
 
     public async Task<Weather?> GetWeatherAsync(string city)
     {
+        var cachedResult = await _redis.StringGetAsync(city);
+
+        if (!string.IsNullOrEmpty(cachedResult))
+        {
+            Console.WriteLine($"Cached: {city}");
+            return JsonSerializer.Deserialize<Weather>(cachedResult);
+        }
+
         var url = $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?unitGroup=metric&key={_apiKey}&contentType=json";
 
         try
         {
             var response = await _httpClient.GetFromJsonAsync<Weather>(url);
+
+            if (response != null)
+            {
+                Console.WriteLine($"Not Cached: {city}");
+                var json = JsonSerializer.Serialize(response);
+                await _redis.StringSetAsync(city, json, TimeSpan.FromHours(1));
+            }
+
             return response;
         }
         catch (HttpRequestException ex)
